1f0e35866c8e1f662df32eed0ae780e101d5e818
ismailgungor <ismail.gungor@adesso.com.tr>
2022-08-08 02:45:09 +0300

    Added core app

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Sample Tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    
+    def test_add_numbers(self):
+        res = calc.add(5,6)
+        self.assertEquals(res,11)
+
+
+ 
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,9 @@
+"""
+Test custom Django management commands.
+"""
+
+ from unittest.mock import patch
+from psycopg2 import OperationalError as Psycop2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
