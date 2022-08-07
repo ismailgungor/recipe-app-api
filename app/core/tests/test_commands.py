6c26e50695aa0bf0daed2a8b808d10ddbd23d006
ismailgungor <ismail.gungor@adesso.com.tr>
2022-08-08 02:46:09 +0300

    Added core app v2

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,7 +2,7 @@
 Test custom Django management commands.
 """
 
- from unittest.mock import patch
+from unittest.mock import patch
 from psycopg2 import OperationalError as Psycop2Error
 
 from django.core.management import call_command
