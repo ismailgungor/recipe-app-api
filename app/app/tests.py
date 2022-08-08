3e47142280fff732c6993c2d8b3572bd53f69f77
ismailgungor <ismail.gungor@adesso.com.tr>
2022-08-09 02:02:29 +0300

    Modified in order to set up database

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,15 +2,13 @@
 Sample Tests
 """
 
-from django.test import SimpleTestCase
 
+from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-    
+
     def test_add_numbers(self):
-        res = calc.add(5,6)
-        self.assertEquals(res,11)
-
-
- 
+        res = calc.add(5, 6)
+        self.assertEquals(res, 11)
