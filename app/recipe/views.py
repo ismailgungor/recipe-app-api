f02f363b6528cffac05efc6a154d190d111586cb
ismailgungor <ismail.gungor@adesso.com.tr>
2022-08-23 02:24:26 +0300

    Last changes

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -30,7 +30,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return the serializer class for request."""
         if self.action == 'list':
             return serializers.RecipeSerializer
-        elif self.action == 'upload-image':
+        elif self.action == 'upload_image':
             return serializers.RecipeImageSerializer
 
         return self.serializer_class
@@ -39,7 +39,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new Recipe"""
         serializer.save(user=self.request.user)
 
-    @action(methods=['POST'], detail=True, url_path='upload_image')
+    @action(methods=['POST'], detail=True, url_path='upload-image')
     def upload_image(self, request, pk=None):
         """Upload an image to recipe."""
         recipe = self.get_object()
